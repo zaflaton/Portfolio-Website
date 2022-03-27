e13859ab976e64a71570533d39cab92f632c7269
zaflaton <59338890+zaflaton@users.noreply.github.com>
2022-03-27 22:31:33 +0800

    init commit

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-$(document).ready(function(){
+let menu = document.querySelector('#menu-btn');
+let home = document.querySelector('.home');
+let header = document.querySelector('.header');
 
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
-
-  $(window).on('scroll load',function(){
-
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
-
-  });
-
-  // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
-
-    e.preventDefault();
-
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
-      500, 
-      'linear'
-    );
-
-  });
-
-});
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  header.classList.toggle('active');
+  home.classList.toggle('active');
+};
