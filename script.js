b7978ab5dc67eee9cb5a4bfb2d582ca684f0b4bb
zaflaton <59338890+zaflaton@users.noreply.github.com>
2022-03-29 10:59:52 +0800

    add animation

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ menu.onclick = () => {
   header.classList.toggle('active');
   home.classList.toggle('active');
 };
+
+window.onscroll = () => {
+  if (window.innerWidth < 991) {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+    document.body.classList.remove('active');
+  }
+
+  document.querySelectorAll('section').forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      document.querySelectorAll('.header .navbar a').forEach((links) => {
+        links.classList.remove('active');
+        document
+          .querySelector('.header .navbar a[href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
+};
